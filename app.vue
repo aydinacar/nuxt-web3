6445fd0455b30199bbfb90080a842bb51ec76a1f
acaraydin01 <acaraydin01@gmail.com>
2022-05-20 14:53:40 +0300

    updated app

--- app.vue
+++ app.vue
@@ -1,20 +1,8 @@
 <template>
   <div>
-    <AppHeader />
     <NuxtPage />
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import AppHeader from "./components/AppHeader.vue";
-export default defineComponent({
-  components: {
-    AppHeader,
-  },
-  setup() {},
-});
-</script>
 
 <style>
 * {
